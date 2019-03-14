417e3e0cc6d9e36a11d6b165c9bccea4f753d7f3
Benedikt Ritter <benedikt@gradle.com>
2019-03-14 17:11:30 +0100

    Init gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:27.0.1-jre")
+
+    testImplementation("junit:junit:4.12")
+}
+
+application {
+    mainClassName = "com.gradle.simple.App"
+}
