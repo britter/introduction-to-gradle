66d338446a1aebe8a0c2897542fa64ae44488fe7
Benedikt Ritter <benedikt@gradle.com>
2019-03-14 19:01:45 +0100

    Add integration test task

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,45 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    create("integrationTest") {
+        java.srcDir(file("src/integration-test/java"))
+        resources.srcDir(file("src/integration-test/resources"))
+        compileClasspath += sourceSets["main"].output
+        runtimeClasspath += sourceSets["main"].output
+    }
+}
+
+val integrationTestImplementation by configurations.getting {
+    extendsFrom(configurations["testImplementation"])
+}
+
+configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
+
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:2.1.3.RELEASE"))
     implementation("com.google.guava:guava:27.0.1-jre")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     testImplementation("junit:junit:4.12")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    integrationTestImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.register<Zip>("zipSources") {
     archiveClassifier.set("src")
     from(sourceSets.main.get().allJava)
+}
+
+val integrationTest = tasks.register<Test>("integrationTest") {
+    description = "Runs the integration tests."
+    group = "verification"
+
+    testClassesDirs = sourceSets["integrationTest"].output.classesDirs
+    classpath = sourceSets["integrationTest"].runtimeClasspath
+    mustRunAfter(tasks["test"])
+}
+
+tasks.check {
+    dependsOn(integrationTest)
 }
