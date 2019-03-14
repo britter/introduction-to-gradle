a11c7cde19548dc639b8cffbb82f0a5b50657462
Benedikt Ritter <benedikt@gradle.com>
2019-03-14 20:04:05 +0100

    Extract integration tests configuration into separate build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,13 @@ plugins {
     java
 }
 
+apply(from = "gradle/integration-test.gradle.kts")
+
 repositories {
     mavenCentral()
 }
 
-sourceSets {
-    create("integrationTest") {
-        java.srcDir(file("src/integration-test/java"))
-        resources.srcDir(file("src/integration-test/resources"))
-        compileClasspath += sourceSets["main"].output
-        runtimeClasspath += sourceSets["main"].output
-    }
-}
-
-val integrationTestImplementation by configurations.getting {
-    extendsFrom(configurations["testImplementation"])
-}
-
-configurations["integrationTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
+val integrationTestImplementation by configurations.getting
 
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:2.1.3.RELEASE"))
@@ -34,17 +23,4 @@ dependencies {
 tasks.register<Zip>("zipSources") {
     archiveClassifier.set("src")
     from(sourceSets.main.get().allJava)
-}
-
-val integrationTest = tasks.register<Test>("integrationTest") {
-    description = "Runs the integration tests."
-    group = "verification"
-
-    testClassesDirs = sourceSets["integrationTest"].output.classesDirs
-    classpath = sourceSets["integrationTest"].runtimeClasspath
-    mustRunAfter(tasks["test"])
-}
-
-tasks.check {
-    dependsOn(integrationTest)
 }
